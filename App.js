78e1cf48a0ef466affd471c925955edbbfe7581e
itsofficial.rahul <98083485+itsofficial-rahul@users.noreply.github.com>
2022-04-07 11:27:57 +0530

    changes in input filed ( add sunrise,sunset,wind )

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
+
+const moment = require("moment");
 const apiKey = {
   key: "d346be2d71078b4aa955c15f7601dd38",
   base: "https://home.openweathermap.org/api_keys",
@@ -8,11 +10,11 @@ const apiKey = {
 function App() {
   const [wetherdata, setwetherdata] = useState([]);
   const [loading, setloading] = useState(true);
-  const [serch, setserch] = useState("London");
-  const [state, setsate] = useState("Londen");
+  const [search, setsearch] = useState("London");
+  const [city, setCity] = useState("");
   const [error, seterror] = useState(false);
 
-  const data = `https://api.openweathermap.org/data/2.5/weather?q=${serch}&appid=d346be2d71078b4aa955c15f7601dd38`;
+  const data = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=d346be2d71078b4aa955c15f7601dd38`;
   const fetchapi = async () => {
     const data1 = await fetch(data);
     const data2 = await data1.json();
@@ -27,20 +29,13 @@ function App() {
   };
   function showdata(e) {
     e.preventDefault();
-    setsate(serch);
+    setCity(search);
   }
-
-  var d = new Date();
-  var n = d.toLocaleString([], {
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true,
-  });
-  const dat = d.toLocaleString().split(",")[0];
 
   useEffect(() => {
     fetchapi();
-  }, [state]);
+  }, [city]);
+  // console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
   return (
     <div className="App">
       <div className="InputField">
@@ -50,9 +45,11 @@ function App() {
             placeholder="Enter Cityname..."
             id="input2"
             type="text"
-            onChange={(e) => setserch(e.target.value)}
+            pattern="[a-z/A-Z]{1,12}"   // only string data
+            title="City Not Found"
+            onChange={(e) => setsearch(e.target.value)}
           ></input>
-          {error ? <h5 id="error_Showing">City Not Found..</h5> : null}
+
           <button
             onChange={(e) => e.preventDefault()()}
             className="btn"
@@ -60,6 +57,7 @@ function App() {
           >
             show
           </button>
+          {error ? <h5 id="error_Showing">City Not Found..</h5> : null}   // for error showing
         </form>
         {loading ? (
           <h1>Loading...</h1>
@@ -70,13 +68,18 @@ function App() {
                 <h1>{alldata.name}</h1>
 
                 <h2>
-                  {(alldata.main.temp - 273 + 1).toFixed(1)}
+                  {+(alldata.main.temp - 273 - 0.2).toFixed(1)}
                   &deg;Cel
                 </h2>
-                <h3>
-                  {dat} &nbsp;&nbsp;
-                  {n}
-                </h3>
+                <h3>{moment().format(" Do MMMM YYYY  LT")}</h3>
+                <h4>
+                  {" "}
+                  SunRise : {moment(alldata.sys.sunrise * 1000).format("LT")}  // best for time
+                </h4>
+                <h4>
+                  SunSet : {moment(alldata.sys.sunset * 1000).format("LT")}  // Time formsster sun rise ka
+                </h4>
+                <h4>Wind Speed : {alldata.wind.speed} h /km</h4>
               </div>
             );
           })
